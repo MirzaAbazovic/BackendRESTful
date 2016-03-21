a920b6e5d34cfe567cf6b9910a9e6fc8673f05d8
Programiraj.ba <programiraj.ba@gmail.com>
2016-03-21 14:21:15 +0100

    dodao bower i datatables za sminku tabele

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,28 @@
- var socket = io('http://localhost:8080');
-  socket.on('connect', function(){
-    console.log("Connected");  
+
+
+$(document).ready(function() {
+    var table = $('#log-table').DataTable();
+    var socket = io('http://localhost:8080');
+    socket.on('event', function(data) {
+        console.log(data);
+        table.row.add([
+            data.entity,
+            data.action,
+            JSON.stringify(data.data,null, 2)            
+        ]).draw(false);
+        /*
+        var tableRef = document.getElementById('log-table').getElementsByTagName('tbody')[0];
+        var newRow = tableRef.insertRow(tableRef.rows.length);
+        var entityCell = newRow.insertCell(0);
+        var entityText = document.createTextNode(data.entity);
+        var actionCell = newRow.insertCell(1);
+        var actionText = document.createTextNode(data.action);
+        var dataCell = newRow.insertCell(2);
+        var dataText = document.createTextNode(JSON.stringify(data.data));
+        entityCell.appendChild(entityText);
+        actionCell.appendChild(actionText);
+        dataCell.appendChild(dataText);
+        */
     });
-  socket.on('event', function(data){
-      console.log(data);
-      var tableRef = document.getElementById('log-table').getElementsByTagName('tbody')[0];
-      var newRow   = tableRef.insertRow(tableRef.rows.length);
-      var entityCell  = newRow.insertCell(0);
-      var entityText  = document.createTextNode(data.entity);
-      var actionCell  = newRow.insertCell(1);
-      var actionText  = document.createTextNode(data.action);
-      var dataCell  = newRow.insertCell(2);
-      var dataText  = document.createTextNode(JSON.stringify(data.data));
-      entityCell.appendChild(entityText);
-      actionCell.appendChild(actionText);
-      dataCell.appendChild(dataText);      
-  });
-  socket.on('disconnect', function(){
-      console.log("Disconnected");
-  });
+});
+
